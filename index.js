a4f22c306384204c73cc945251ad5d376e3a40f2
Carta447 <tiktokdoraull@gmail.com>
2025-08-04 09:20:16 -0300

    feat: fazendo validação de login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const alunosRoutes = require('./routes/alunos');
 const colaboradoresRoutes = require('./routes/colaboradores');
+const usuariosRoutes = require('./routes/usuarios');
 
 const app = express();
 const PORT = 3001;
@@ -11,6 +12,11 @@ app.use(express.json());
 
 app.use('/alunos', alunosRoutes);
 app.use('/colaboradores', colaboradoresRoutes);
+app.use('/usuarios', usuariosRoutes);
+
+app.get('/test', (req, res) => {
+  res.json({ message: 'Servidor funcionando!' });
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
