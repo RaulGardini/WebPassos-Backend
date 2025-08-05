5e6693baac5a0fc5ce44129cf061fb0f794069d7
Carta447 <tiktokdoraull@gmail.com>
2025-08-05 12:13:49 -0300

    feat: tela de cadastro de turmas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const alunosRoutes = require('./routes/alunos');
 const colaboradoresRoutes = require('./routes/colaboradores');
 const usuariosRoutes = require('./routes/usuarios');
+const turmasRoutes = require('./routes/turmas');
 
 const app = express();
 const PORT = 3001;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/alunos', alunosRoutes);
 app.use('/colaboradores', colaboradoresRoutes);
 app.use('/usuarios', usuariosRoutes);
+app.use('/turmas', turmasRoutes);
 
 app.get('/test', (req, res) => {
   res.json({ message: 'Servidor funcionando!' });
